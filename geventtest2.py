90d909f4d42aeedd857e8a6a3bd125891794a398
zgsoft <zgsoft@sina.com>
2018-01-28 11:27:51 +0800

    创建文件20180128 by Hmily

---
+++ geventtest2.py
@@ -0,0 +1,12 @@
+from gevent import monkey; monkey.patch_all()
+import gevent
+import urllib2
+def f(url):
+	print 'Get %s....'%url
+	resp=urllib2.urlopen(url)
+	data=resp.read()
+	print ('%d bytes received from %s' %(len(data),url))
+gevent.joinall([
+	      gevent.spawn(f,'https://www.python.org/'),
+	      gevent.spawn(f,'https://www.sina.com.cn/'),
+	      gevent.spawn(f,'https://github.com/')])
